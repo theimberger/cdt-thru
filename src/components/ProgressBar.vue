040305875d83b7d9150157f5a348190ccc286f38
t <theimberger@gmail.com>
2026-04-08 10:19:11 -0400

    Delete templates, begin building progress bar

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+  <!-- <DocumentationIcon /> -->
+  <div class='progress-bar'>
+  </div>
+</template>
+
+<style scoped>
+.progress-bar {
+  position: absolute;
+  left: 24px;
+  top: 24px;
+  bottom: 24px;
+  border: 2px solid black;
+  border-radius: 12px;
+  width: 24px;
+}
+
+@media (min-width: 800px) {
+  .progress-bar {
+    position: absolute;
+    right: 24px;
+    top: 24px;
+    border: 2px solid black;
+    border-radius: 12px;
+    height: 24px;
+    width: auto;
+    bottom: auto;
+  }
+}
+</style>
